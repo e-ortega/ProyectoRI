2eedcd925fad981588b8e69a918387ed47b95546
Josue Valverde Sanchez <b16802@ecci.ucr.ac.cr>
2018-09-14 10:05:14 -0600

    Modificacion de calculo de frecuencia

--- frecuencia.py
+++ frecuencia.py
@@ -1,15 +1,21 @@
-def main():
-    lista = [10, 15, 79, 45, 314, 54, 54, 47]
-    list1 = []
-    for item in lista:
-        print item
-    FreNormal(list1, lista)
-
+from fractions import Fraction
 
 def FreNormal(listapalabras, listafrecuencias):
     vectorfecnormal = []
     frecmax = max(listafrecuencias)
     for frec in listafrecuencias:
-        valornormal = frec/frecmax
+        print('Fre ',  frec,' Fmax ', frecmax)
+        valornormal = frec / frecmax#Fraction(frec, frecmax)
+        print(valornormal)
         vectorfecnormal.append(valornormal)
     print vectorfecnormal
+
+
+
+lista = [10, 15, 79, 45, 314, 54, 54, 47]
+list1 = []
+for item in lista:
+    print item
+FreNormal(list1, lista)
+
+

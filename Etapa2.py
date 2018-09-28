5766d52f86bcb157d0eba94d9caeef59b25f684d
unknown <eortega1793@gmail.com>
2018-09-27 19:32:07 -0600

    avance en trato de palabras

--- Etapa2.py
+++ Etapa2.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import os
+import re
 import requests
+from http import HTTPStatus
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 
 # este es el vector de palabras que se llena con palabras que se deben omitir en los textos
 stop_words = []
+plain_text_dir = "plain_text"
+path = os.path.dirname(os.path.realpath(__file__))
 
 
 def run():
+    # create your subdirectory
+    if not os.path.exists(os.path.join(path, plain_text_dir)):
+        os.mkdir(os.path.join(path, plain_text_dir))
+
     fill_stop_words()
     read_urls()
 
@@ -27,31 +35,35 @@ def fill_stop_words():
 def read_urls():
     file_name = "urles.txt"
     file = open(file_name, "r", encoding="utf-8")
-    # counter = 0
-    separator = []
     for line in file:
+        separator = []
         for word in line.split():
             separator.append(word)
-            # counter += 1
         html_parser(separator)
-    # tokenizer("archivo.txt")
 
 
 # substrae el texto de un html y lo guarda en un archivo
 def html_parser(separator):
-    print(separator[1])
-    page = requests.get(separator[1])
     name = separator[0]
-    soup = BeautifulSoup(page.content, 'html.parser')
-    #text = soup.get_text()
-    text = soup.findAll(text = True)
-    visible_texts = filter(tag_visible, text)
-    texts = u" ".join(t.strip() for t in visible_texts)
-    full_text = name + '.txt'
-    file_text = open(full_text, '+w', encoding="utf-8")
-    file_text.write(texts)
-    file_text.close()
-    tokenizer(full_text)
+    page = requests.get(separator[1])
+
+    if page.status_code == HTTPStatus.OK:
+        soup = BeautifulSoup(page.content, 'html.parser')
+        text = soup.findAll(text=True)
+        visible_texts = filter(tag_visible, text)
+        texts = u" ".join(t.strip() for t in visible_texts)
+
+        file_name = name + '.txt'
+        path_file = os.path.join(path, plain_text_dir, file_name)
+
+        # create an empty file.
+        try:
+            file_text = open(path_file, '+w', encoding="utf-8")
+            file_text.write(texts)
+            file_text.close()
+            tokenizer(path_file)
+        except IOError:
+            print("Algo pasó creando el archivo el documento: [%s].", file_name)
 
 
 def tag_visible(element):
@@ -64,24 +76,37 @@ def tag_visible(element):
 
 # lee el archivo sin bloques html dado por htmlParser, recorre cada palabra, la busca en stop_words, si no la encuentra
 # la busca en el vector de palabras, si la encuentra le suma 1 en la posicion de caso contrario la agrega con un 1
-def tokenizer(filename):
+def tokenizer(file_name):
     word_list = []
     frequency_list = []
-    # filename = archivo
-    file = open(filename, "r", encoding="utf-8")
+    file = open(file_name, "r", encoding="utf-8")
     for line in file:
         for word in line.split():
-            if not stop_words.__contains__(word):
-                if word_list.__contains__(word):
-                    frequency_list[word_list.index(word)] += 1
-                else:
-                    word_list.append(word)
-                    frequency_list.append(1)
-    # i = 0
-    # for j in word_list:
-    #    print(word_list[i] + " " + str(frequency_list[i]))
-    #    i += 1
-    normalize_frequency(filename, word_list, frequency_list)
+            try:
+                if word=="中文":
+                    print("d")
+                valid_word = True
+                word = word.lower()
+                word = re.sub(r'\W+', '', word)  # se eliminan caracteres no alfanumericos
+                if not word.isalnum():
+                    valid_word = False
+                if len(word) < 2 or len(word) > 30:
+                    print("[%s] longitud de palaba no permitida" % word)
+                    valid_word = False  # no se usan palabras de tamaño mayor a 30
+                if not word.isdigit() and not word[0].isalpha():
+                    print("[%s] no es una palabra valida" % word)
+                    valid_word = False# no se usan palabras que no sean solo numeros y no empiecen por a-z ej: 3arbol
+
+                if not stop_words.__contains__(word) and valid_word:
+                    if word_list.__contains__(word):
+                        frequency_list[word_list.index(word)] += 1
+                    else:
+                        word_list.append(word)
+                        frequency_list.append(1)
+            except Exception as error:
+                print("[%s] %s" % (word, error))
+                print(error)
+    normalize_frequency(file_name, word_list, frequency_list)
 
 
 # crea vectores con las frecuencias relativas de los terminos de un documneto
@@ -97,15 +122,18 @@ def normalize_frequency(file_name, word_list, frequency_list):
 # crea un archivo .tok a partir de los vectores de palabras y frecuencias normalizadas
 def create_tok(file_name, word_list, normalized_frequency_list, frequency_list):
     fulltext = file_name + '.tok'
-    file_text = open(fulltext, '+w', encoding="utf-8")
-    vectorStr = [str(i) for i in normalized_frequency_list]
-    vectorFrec = [str(i) for i in frequency_list]
-    counter = 0
-    for i in word_list:
-        file_text.write(word_list[counter] + "  " + vectorStr[counter] + "   " + vectorFrec[counter] + "\n")
-        counter = counter + 1
 
-    file_text.close()
+    try:
+        file_text = open(fulltext, '+w', encoding="utf-8")
+        vector_str = [str(i) for i in normalized_frequency_list]
+        vector_frequency = [str(i) for i in frequency_list]
+        counter = 0
+        for word in word_list:
+            file_text.write(word + "  " + vector_str[counter] + "   " + vector_frequency[counter] + "\n")
+            counter += 1
+        file_text.close()
+    except IOError:
+        print("Algo pasó creando el .tok para el documento: [%s].", file_name)
 
 
 # crea el archivo vabulario

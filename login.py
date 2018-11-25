adce0fc1fa77cfed3d06319242eb202446c74554
jvs1991 <josue.valverde1@gmail.com>
2018-11-24 22:00:24 -0600

    interfaz web V2

--- login.py
+++ login.py
@@ -1,19 +1,36 @@
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
+listURL = []
+htmlString = r'<p>Escriba su consulta:<input type = "text" name = "nm" /><input type = "submit" value = "Buscar" /></p>'
+htmlbr = "<br>"
+numPos = 1
+
 
 
 @app.route('/success/<name>')
 def success(name):
     #return 'welcome %s' % name
-    str2 = r'<p>Escriba su consulta:<input type = "text" name = "nm" /><input type = "submit" value = "Buscar" /></p>'
-    str3 = r'<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a> ' + "<br>" + r'<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a> '
-    str2 = str2 + str3
-    return str2
+    #str2 = r'<p>Escriba su consulta:<input type = "text" name = "nm" /><input type = "submit" value = "Buscar" /></p>'
+    #str3 = r'<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a> ' + "<br>" + r'<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a> '
+    strrespon = htmlString
+    i= 9*numPos-9
+    for x in range(i, 9*numPos):
+        strrespon += listURL[i]
+        strrespon += htmlbr
+    numberPos = numPos+10
+    return strrespon
+
+
+def fillList():
+    for x in range(0, 14):
+        temp = r'<a href="https://www.w3schools.com/html/">'+str(x)+r'</a> '
+        listURL.append(temp)
 
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        fillList()
         user = request.form['nm']
         return redirect(url_for('success',name = user))
     else:

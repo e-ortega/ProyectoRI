33c4183b7dbfc9f95a21ff6478d091f716f7d10b
jvs1991 <josue.valverde1@gmail.com>
2018-11-26 10:38:31 -0600

    Cambios en la carga de datos

--- login.py
+++ login.py
@@ -7,26 +7,33 @@ htmlbr = "<br>"
 botonNext = r'<button type="submit" value="Next">Siguiente</button> '
 botonPrev = r'<button type="submit" value="Prev">Previo</button><div class="divider"/>'
 htmlend = r'</body></html>'
-numPos = 1
+numPos = -1
 
 
 
-@app.route('/success/<name>')
-def success(name):
-    #return 'welcome %s' % name
-    #str2 = r'<p>Escriba su consulta:<input type = "text" name = "nm" /><input type = "submit" value = "Buscar" /></p>'
-    #str3 = r'<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a> ' + "<br>" + r'<a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a> '
+@app.route('/success/<name>/<y>')
+def success(name,y):
     strrespon = htmlhead
     strrespon += htmlString
-    i= 9*numPos-9
-    for x in range(i, 9*numPos):
-        strrespon += listURL[x]
+    numPos = int(y)
+    x = 9 + numPos
+    i = x-9
+    while i <= x and i < len(listURL):
+        strrespon += listURL[i]
         strrespon += htmlbr
+        i += 1
+    #for x in range(i, 9*numPos):
+    #   strrespon += listURL[x]
+    #  strrespon += htmlbr
     strrespon +=htmlend
     strrespon += botonPrev
     strrespon +=botonNext
     numberPos = numPos+10
     return strrespon
+
+
+def changePage():
+    return "h"
 
 
 def fillList():
@@ -40,10 +47,10 @@ def login():
     if request.method == 'POST':
         fillList()
         user = request.form['nm']
-        return redirect(url_for('success',name = user))
+        return redirect(url_for('success',name = user,y=0))
     else:
         user = request.args.get('nm')
-        return redirect(url_for('success',name = user))
+        return redirect(url_for('success',name = user,y=0))
 
 
 if __name__ == '__main__':

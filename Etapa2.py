9b4d6d4923aff5b3b4bbf9a18f9742d268d9173b
unknown <eortega1793@gmail.com>
2018-11-26 20:59:53 -0600

    arreglado

--- Etapa2.py
+++ Etapa2.py
@@ -47,7 +47,7 @@ def fill_stop_words():
 
 # lee de un archivo y debe substraer el url al que se quiere ingresar y el nombre del archivo
 def read_urls():
-    file_name = "urles.txt"
+    file_name = "URLS.txt"
     file = open(file_name, "r", encoding="utf-8")
     for line in file:
         separator = []
@@ -60,8 +60,6 @@ def read_urls():
 def html_parser(separator):
     name = str(separator[0])
     name = name.replace(u'\ufeff', '')
-    file_name = name + '.txt'
-    file_name = file_name.replace(".html", "")
     file_dir = os.path.dirname(__file__)
 
     abs_file_path = file_dir + "/Coleccion" + '/' + name
@@ -70,17 +68,18 @@ def html_parser(separator):
     soup = BeautifulSoup(page, 'html.parser')
     text = soup.findAll(text=True)
     visible_texts = filter(tag_visible, text)
-    texts = u" ".join(t.strip() for t in visible_texts)
-    path_file = os.path.join(path, plain_text_dir, file_name)
+    clean_text = u" ".join(t.strip() for t in visible_texts)
 
+    tokenizer(clean_text)
+    # path_file = os.path.join(path, plain_text_dir, file_name)
     # create an empty file.
-    try:
-        file_text = open(path_file, '+w', encoding="utf-8")
-        file_text.write(texts)
-        file_text.close()
-        tokenizer(path_file)
-    except IOError:
-        print("Algo pasó creando el archivo el documento: [%s].", file_name)
+    # try:
+    #     file_text = open(path_file, '+w', encoding="utf-8")
+    #     file_text.write(texts)
+    #     file_text.close()
+    #     tokenizer(path_file)
+    # except IOError:
+    #     print("Algo pasó creando el archivo el documento: [%s].", file_name)
 
 
 
@@ -94,43 +93,42 @@ def tag_visible(element):
 
 # lee el archivo sin bloques html dado por htmlParser, recorre cada palabra, la busca en stop_words, si no la encuentra
 # la busca en el vector de palabras, si la encuentra le suma 1 en la posicion de caso contrario la agrega con un 1
-def tokenizer(file_name):
+def tokenizer(clean_text):
     word_list = []
     frequency_list = []
-    file = open(file_name, "r", encoding="utf-8")
+    # file = open(file_name, "r", encoding="utf-8")
 
-    for line in file:
-        for word in line.split():
-            try:
+    # for line in file:
+    for word in clean_text.split():
+        try:
+            valid_word = True
+            word = word.lower()
+            word = re.sub(r'\W+', '', word)  # se eliminan caracteres no alfanumericos
+            if not word.isalnum():
+                valid_word = False
+            if len(word) < 2 or len(word) > 30:
+                valid_word = False  # no se usan palabras de tamaño mayor a 30
+            if len(word) > 0:
+                if not word.isdigit() and not word[0].isalpha():
+                    valid_word = False  # no se usan palabras que no sean solo numeros y no empiecen por a-z ej: 3arbol
 
-                valid_word = True
-                word = word.lower()
-                word = re.sub(r'\W+', '', word)  # se eliminan caracteres no alfanumericos
-                if not word.isalnum():
-                    valid_word = False
-                if len(word) < 2 or len(word) > 30:
-                    valid_word = False  # no se usan palabras de tamaño mayor a 30
-                if len(word) > 0:
-                    if not word.isdigit() and not word[0].isalpha():
-                        valid_word = False  # no se usan palabras que no sean solo numeros y no empiecen por a-z ej: 3arbol
+            if not stop_words.__contains__(word) and valid_word:
+                if word_list.__contains__(word):
+                    frequency_list[word_list.index(word)] += 1
+                else:
+                    word_list.append(word)
+                    frequency_list.append(1)
 
-                if not stop_words.__contains__(word) and valid_word:
-                    if word_list.__contains__(word):
-                        frequency_list[word_list.index(word)] += 1
-                    else:
-                        word_list.append(word)
-                        frequency_list.append(1)
+                if global_words_list.__contains__(word):
+                    total_frequency_list[global_words_list.index(word)] += 1
+                else:
+                    document_quantity_list.append(0)
+                    global_words_list.append(word)
+                    total_frequency_list.append(1)
 
-                    if global_words_list.__contains__(word):
-                        total_frequency_list[global_words_list.index(word)] += 1
-                    else:
-                        document_quantity_list.append(0)
-                        global_words_list.append(word)
-                        total_frequency_list.append(1)
-
-            except Exception as error:
-                print("[" + str(word) + "]" + error.__str__())
-                requests_errors.append(file_name)
+        except Exception as error:
+            print("[" + str(word) + "]" + error.__str__())
+            requests_errors.append(file_name)
     for word in word_list:
         document_quantity_list[global_words_list.index(word)] += 1
     normalize_frequency(file_name, word_list, frequency_list)
@@ -160,6 +158,7 @@ def create_tok(file_name, word_list, normalized_frequency_list, frequency_list):
     file_name = file_name.replace(".txt", "")
     index = file_name.find('plain_text')
     file = file_name[:index + len('plain_text')] + r'\tok' + file_name[index + len('plain_text'):]
+
     for passnum in range(1, len(word_list)):
         i = 0
         for element in range(0, len(word_list) - passnum):
@@ -182,19 +181,19 @@ def create_tok(file_name, word_list, normalized_frequency_list, frequency_list):
         counter = 0
         for word in word_list:
             # file_text.write(word + "," + vector_str[counter] + "," + vector_frequency[counter] + "\n")
-            file_text.write(word + " " * (30 - len(word)) + ", " + vector_str[counter]+ " " + " " * (
-                        12 - len(vector_str[counter])) + " , " + vector_frequency[counter] + " " * (
-                                        20 - len(vector_frequency[counter])) + "\n")
+            file_text.write(word + " " * (30 - len(word)) + ", " + vector_str[counter] + " " + " " * (
+                    12 - len(vector_str[counter])) + " , " + vector_frequency[counter] + " " * (
+                                    20 - len(vector_frequency[counter])) + "\n")
             counter += 1
         file_text.close()
     except IOError:
         print("Algo pasó creando el .tok para el documento: [%s].", file_name)
 
 
-#Hace un sort para 3 lista, tomando como ordenamiento la lista base, este algorito es utilizado para ordenar el vocabulario
+# Hace un sort para 3 lista, tomando como ordenamiento la lista base, este algorito es utilizado para ordenar el vocabulario
 def merge_sort_3_listas(lista_base, lista1, lista2):
-    if len(lista_base)>1:
-        medio = len(lista_base)//2
+    if len(lista_base) > 1:
+        medio = len(lista_base) // 2
         mitad_iz_base = lista_base[:medio]
         mitad_de_base = lista_base[medio:]
 
@@ -207,36 +206,36 @@ def merge_sort_3_listas(lista_base, lista1, lista2):
         merge_sort_3_listas(mitad_iz_base, mitad_iz1, mitad_iz2)
         merge_sort_3_listas(mitad_de_base, mitad_de1, mitad_de2)
 
-        i=0
-        j=0
-        k=0
+        i = 0
+        j = 0
+        k = 0
 
         while i < len(mitad_iz_base) and j < len(mitad_de_base):
             if mitad_iz_base[i] < mitad_de_base[j]:
                 lista_base[k] = mitad_iz_base[i]
                 lista1[k] = mitad_iz1[i]
                 lista2[k] = mitad_iz2[i]
-                i=i+1
+                i = i + 1
             else:
                 lista_base[k] = mitad_de_base[j]
                 lista1[k] = mitad_de1[j]
                 lista2[k] = mitad_de2[j]
-                j=j+1
-            k=k+1
+                j = j + 1
+            k = k + 1
 
         while i < len(mitad_iz_base):
             lista_base[k] = mitad_iz_base[i]
             lista1[k] = mitad_iz1[i]
             lista2[k] = mitad_iz2[i]
-            i=i+1
-            k=k+1
+            i = i + 1
+            k = k + 1
 
         while j < len(mitad_de_base):
             lista_base[k] = mitad_de_base[j]
             lista1[k] = mitad_de1[j]
             lista2[k] = mitad_de2[j]
-            j=j+1
-            k=k+1
+            j = j + 1
+            k = k + 1
 
 
 # crea el archivo vabulario
@@ -245,14 +244,14 @@ def create_vocabulary():
     file_name = os.path.join(path, plain_text_dir, file_name)
 
     merge_sort_3_listas(global_words_list, total_frequency_list, document_quantity_list)
-    
+
     try:
         file_text = open(file_name, '+w', encoding="utf-8")
         vector_frequency = [str(i) for i in total_frequency_list]
         vector_quantity = [str(i) for i in document_quantity_list]
         counter = 0
         for word in global_words_list:
-            file_text.write(str(word) + " " + ", " + str(vector_frequency[counter])+ " " + ", " + str(vector_quantity[counter]) + "\n")
+            file_text.write(str(word) + " " + ", " + str(vector_frequency[counter]) + " " + ", " + str(vector_quantity[counter]) + "\n")
             counter += 1
         file_text.close()
     except IOError:
@@ -272,7 +271,7 @@ def generate_error_page_file():
         print("Algo pasó creando el .tok para el documento: [%s].", file_name)
 
 
-#Crea in Indice a partir del archivo posting
+# Crea in Indice a partir del archivo posting
 def indice(archivo):
     archivo = archivo.replace(".\\", "\\")
     file_name = 'Indice.txt'
@@ -290,22 +289,23 @@ def indice(archivo):
         print(e)
     c = 0
     for l in lines:
-        pos = l.find(" ")
-        #pos = l.find(",")
+		# probar al contrario
+        # pos = l.find(" ")
+        pos = l.find(",")
         lines[c] = l[:pos]
-        c = c+1
-    termino=" "
-    c=0
+        c = c + 1
+    termino = " "
+    c = 0
     try:
         file_text = open(file_name, '+w', encoding="utf-8")
         numeroVeces = 0
         for l in lines:
-          numeroVeces += 1
-          if (termino != l):
+            numeroVeces += 1
+            if (termino != l):
                 termino = l
-                file_text.write(termino + " " * (30-len(termino)) + ", " + str(c) + " " * (12-len(str(c))) + ", " + str(numeroVeces)+"\n")
+                file_text.write(termino + " " * (30 - len(termino)) + ", " + str(c) + " " * (12 - len(str(c))) + ", " + str(numeroVeces) + "\n")
                 numeroVeces = 0
-          c = c+1
+            c = c + 1
     except IOError:
         print("Algo pasó creando el archivo Indice")
 
@@ -315,10 +315,10 @@ def calculaPesos(frecuenciaNormalizada, frecuenciaInversa):
 
 
 def loadVocabulario(palabras, frecuencias):
-    #file_name = 'vocabulario.txt'
+    # file_name = 'vocabulario.txt'
     cur_path = os.path.dirname(__file__)
     a = cur_path + '/plain_text/Vocabulario.txt'
-    #a = 'C:/Users/Jose M/Google Drive/II Semestre 2018/Proyecto/plain_text/Vocabulario.txt'
+    # a = 'C:/Users/Jose M/Google Drive/II Semestre 2018/Proyecto/plain_text/Vocabulario.txt'
     with open(a, 'r', encoding="utf-8") as file:
         for line in file:
             count = 0
@@ -330,7 +330,7 @@ def loadVocabulario(palabras, frecuencias):
                     frecuencias.append(word)
                     count = count + 1
                 else:
-                    count = count +1
+                    count = count + 1
 
 
 def calculePeso():
@@ -339,8 +339,8 @@ def calculePeso():
     palabrasTok = []
     frecuenciasTok = []
     palabraPeso = []
-    pesos=[]
-    loadVocabulario(palabras,frecuencias)
+    pesos = []
+    loadVocabulario(palabras, frecuencias)
     cur_path = os.path.dirname(__file__)
     tok_path = cur_path + "/plain_text/tok"
     dir_path_name = r'.\plain_text\wtd'
@@ -352,9 +352,9 @@ def calculePeso():
     for (dirpath, dirnames, filenames) in walk(tok_path):
         f.extend(filenames)
         break
-    for n in range (0, len(f)):
+    for n in range(0, len(f)):
 
-        file_name = tok_path + "/"+f[n]
+        file_name = tok_path + "/" + f[n]
         print("creando tok %s", file_name)
         file = open(file_name, 'r', encoding="utf-8")
         for line in file:
@@ -365,20 +365,20 @@ def calculePeso():
                     wordCount = wordCount + 1
                 elif wordCount == 2:
                     frecuenciasTok.append(word)
-                    wordCount = wordCount +1
+                    wordCount = wordCount + 1
                 else:
                     wordCount = wordCount + 1
-        for i in range (0, len(palabrasTok)):
-            for j in range (0, len(palabras)):
+        for i in range(0, len(palabrasTok)):
+            for j in range(0, len(palabras)):
                 if palabras[j] == palabrasTok[i]:
                     palabraPeso.append(palabrasTok[i])
                     pesos.append(float(frecuenciasTok[i]) * float(frecuencias[j]))
-        file_name = cur_path+ "/plain_text/wtd/"+f[n] + ".wtd"
+        file_name = cur_path + "/plain_text/wtd/" + f[n] + ".wtd"
         file_name = file_name.replace(".tok", "")
         file_text = open(file_name, '+w', encoding="utf-8")
-        for k in range (0, len(palabraPeso)):
-            file_text.writelines(str(palabraPeso[k])+ "   "+str(pesos[k]) + "\n")
-            file_posting.writelines(str(palabraPeso[k])+"   "+f[n].replace(".txt.tok","")+ "   "+str(pesos[k]) + "\n")
+        for k in range(0, len(palabraPeso)):
+            file_text.writelines(str(palabraPeso[k]) + "   " + str(pesos[k]) + "\n")
+            file_posting.writelines(str(palabraPeso[k]) + "   " + f[n].replace(".txt.tok", "") + "   " + str(pesos[k]) + "\n")
         file_text.close()
         palabrasTok = []
         frecuenciasTok = []

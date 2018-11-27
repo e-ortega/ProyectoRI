70ba9ef5995e98b83b34adad5cb7521a0e98a0a0
jvs1991 <josue.valverde1@gmail.com>
2018-11-26 19:12:21 -0600

    interfaz web funcional

--- login.py
+++ login.py
@@ -2,45 +2,75 @@ from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 listURL = []
 htmlhead = r'<html> <head> <style>.divider{ width:5px; height:auto; display:inline-block;}</style></head> <body style="background-color:powderblue;">'
-htmlString = r'<center><p>Escriba su consulta:<input type = "text" name = "nm" /><input type = "submit" value = "Buscar" /></p></center>'
+htmlString = r'<form action="/newsearch" method="post"><center><p>Escriba su consulta:<input type = "text" name = "nm" /><input type = "submit" value = "Buscar" /></p></center></form>'
 htmlbr = "<br>"
-botonNext = r'<button type="submit" value="Next">Siguiente</button> '
-botonPrev = r'<button type="submit" value="Prev">Previo</button><div class="divider"/>'
+botonNext = r'<form action="/newPage" method="post"><button type="submit" name="Siguiente" value="Next">Siguiente</button> </form>'
+botonPrev = r'<form action="/oldPage" method="post"><button type="submit" name="Previo" value="Prev">Previo</button></form><div class="divider"/>'
 htmlend = r'</body></html>'
 numPos = -1
+consulta=" "
+
 
 
 
 @app.route('/success/<name>/<y>')
 def success(name,y):
     strrespon = htmlhead
+    global consulta 
+    consulta = name
     strrespon += htmlString
+    global numPos
     numPos = int(y)
-    x = 9 + numPos
+    x = 9 + numPos * 10
     i = x-9
     while i <= x and i < len(listURL):
         strrespon += listURL[i]
         strrespon += htmlbr
         i += 1
-    #for x in range(i, 9*numPos):
-    #   strrespon += listURL[x]
-    #  strrespon += htmlbr
     strrespon +=htmlend
     strrespon += botonPrev
     strrespon +=botonNext
-    numberPos = numPos+10
     return strrespon
+	
+	
+@app.route('/newPage',methods = ['POST', 'GET'])
+def changePageN():
+    if request.method == 'POST':
+			t=int(numPos)+1
+			if (t * 10)<(len(listURL)+10):
+				return redirect(url_for('success',name = consulta,y=t))
+			else:
+				return redirect(url_for('success',name = consulta,y=t-1))
 
+			
+@app.route('/oldPage',methods = ['POST', 'GET'])
+def changePageO():
+    if request.method == 'POST':
+			t=int(numPos)-1
+			if t>=0:
+				return redirect(url_for('success',name = consulta,y=t))
+			else:
+				return redirect(url_for('success',name = consulta,y=t+1))
 
-def changePage():
-    return "h"
-
+			
 
 def fillList():
     for x in range(0, 14):
         temp = r'<a href="https://www.w3schools.com/html/">'+str(x)+r'</a> '
         listURL.append(temp)
 
+		
+@app.route('/newsearch',methods = ['POST', 'GET'])
+def newlogin():
+    if request.method == 'POST':
+        fillList()
+        user = request.form['nm']
+        return redirect(url_for('success',name = user,y=0))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success',name = user,y=0))
+
+		
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
